14520da0a1075717a382a8cda9114363d21d9c3f
DevKDuck <loading95@naver.com>
2024-06-28 21:19:01 +0900

    [JUnit] WebEnvironment.RANDOM_PORT로 랜덤 포트 실행과 insert 쿼리 실행 테스트

---
+++ src/main/java/com/devkduck/springboot/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.devkduck.springboot.service.posts;
+
+import com.devkduck.springboot.domain.posts.PostsRepository;
+import com.devkduck.springboot.web.dto.PostsSaveRequestDto;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/test/java/com/devkduck/springboot/web/PostsApiControllerTest.java
@@ -0,0 +1,56 @@
+package com.devkduck.springboot.web;
+
+import com.devkduck.springboot.domain.posts.Posts;
+import com.devkduck.springboot.domain.posts.PostsRepository;
+import com.devkduck.springboot.web.dto.PostsSaveRequestDto;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PostsApiControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private PostsRepository postsRepository;
+
+    @AfterEach
+    public void tearDown(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void postsAreRegistered()  {
+        String title = "title";
+        String content = "content";
+        PostsSaveRequestDto requestDto = PostsSaveRequestDto.builder()
+                .title(title)
+                .content(content)
+                .author("author")
+                .build();
+        String url = "http://localhost:" + port + "/api/v1/posts";
+
+        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(title);
+        assertThat(all.get(0).getContent()).isEqualTo(content);
+    }
+}
