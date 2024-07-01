2a8b68f954fdb84532e8e6b5903f42a7cc673c49
DevKDuck <loading95@naver.com>
2024-07-01 16:26:13 +0900

    [JUnit] Mustache로 화면 구성하기

---
+++ src/test/java/com/devkduck/springboot/web/IndexControllerTest.java
@@ -0,0 +1,25 @@
+package com.devkduck.springboot.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void mainPageLoading() {
+        // when
+        ResponseEntity<String> response = restTemplate.getForEntity("/", String.class);
+
+        // then
+        assertThat(response.getBody()).contains("Start Web Service");
+    }
+}
