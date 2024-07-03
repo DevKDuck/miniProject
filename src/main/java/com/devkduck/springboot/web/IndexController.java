81b67ffe03d6915f473d87b86c256e515c4ec93e
DevKDuck <loading95@naver.com>
2024-07-03 17:40:55 +0900

    SpringSecurity + Google Login

--- src/main/java/com/devkduck/springboot/web/IndexController.java
+++ src/main/java/com/devkduck/springboot/web/IndexController.java
@@ -1,7 +1,11 @@
 package com.devkduck.springboot.web;
 
+import com.devkduck.springboot.config.auth.LoginUser;
+import com.devkduck.springboot.config.auth.dto.SessionUser;
+import com.devkduck.springboot.domain.user.User;
 import com.devkduck.springboot.service.posts.PostsService;
 import com.devkduck.springboot.web.dto.PostsResponseDto;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,10 +17,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
@@ -31,5 +39,4 @@ public class IndexController {
         model.addAttribute("post", dto);
         return "posts-update";
     }
-
 }
